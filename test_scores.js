8ee487ed35c3525f40153e37fdb8eaff6a98846a
anxicoper <angelicacorzop123@gmail.com>
2025-02-19 10:45:45 -0500

    Update test_scores.js

--- test_scores.js
+++ test_scores.js
@@ -1,125 +1,80 @@
-
 let namesArr = ['Ben', 'Joel', 'Judy', 'Anne'];
 let scoresArr = [88, 98, 77, 88];
 
 function getAvgScore() {
-    let i = 0,
-        sum = 0,
-        len = scoresArr.length;
-    let name = '';
-    for (i; i < len; i++) {
-        sum += scoresArr[i];
-    }
-    return sum / len;
+    let sum = scoresArr.reduce((acc, score) => acc + score, 0);
+    return (sum / scoresArr.length).toFixed(1);
 }
 
 function getHighScore() {
-    let i = 0,
-        max = 0,
-        len = scoresArr.length;
-    let name = '';
-    for (i; i < len; i++) {
-        if (scoresArr[i] > max) {
-            max = scoresArr[i];
-            name = namesArr[i];
-        }
-    }
-    return name + ' with score of ' + max;
+    let maxIndex = scoresArr.indexOf(Math.max(...scoresArr));
+    return `${namesArr[maxIndex]} with a score of ${scoresArr[maxIndex]}`;
 }
 
 function initializeResults() {
-    let results = $('#results');
-    let high = getHighScore();
-    let avg = getAvgScore().toFixed(1);
-    $('#highScore').html(high);
-    $('#avgScore').html(avg);
+    $('#highScore').html(getHighScore());
+    $('#avgScore').html(getAvgScore());
 }
 
-
-function displayScores() {
-    for (let i = 0; i < namesArr.length; i++) {
-        // Assuming namesArr and scoresArr are arrays containing names and scores.
-        $('#scores_table').append('<tr><td>' + namesArr[i] + '</td><td>' + scoresArr[i] + '</td></tr>');
-    }
+function insertTableElement(name, score) {
+    $('#scores_table').append(`<tr><td>${name}</td><td>${score}</td></tr>`);
 }
-
-
-function displayResults() {
-    let results = $('#results');
-    results.toggle();
-}
-
-function insertTableElement(scoresTable, index) {
-    scoresTable.append('<tr><td>' + namesArr[index] + '</td><td>' + scoresArr[index] + '</td></tr>');
-}
-
-
-
 
 function initializeScoresTable() {
-    $('#scores_table tr').slice(1).remove(); 
+    $('#scores_table tr').slice(1).remove();
     for (let i = 0; i < scoresArr.length; i++) {
-        insertNewTableElement(namesArr[i], scoresArr[i]); // Adds the data rows
+        insertTableElement(namesArr[i], scoresArr[i]);
     }
 }
 
+function displayResults() {
+    $('#results').toggle();
+}
 
+function displayScores() {
+    initializeScoresTable();
+    $('#scores').show();
+}
 
 function addScore() {
-    let score = $('#score');
-    let name = $('#name');
-    if (score.val() === '' || name.val() === '') {
-        alert('Name and score must have values');
+    let name = $('#name').val().trim();
+    let score = parseInt($('#score').val(), 10);
+
+    if (!name || isNaN(score) || score < 0 || score > 100) {
+        alert('You must enter a name and a valid score (0-100).');
         return;
     }
-    scoresArr.push(parseInt(score.val()));
-    namesArr.push($("#name").val());
+
+    namesArr.push(name);
+    scoresArr.push(score);
+
     initializeScoresTable();
-    score.val('');
-    name.val('');
     initializeResults();
-    $('#scores').show();
-    $('#results').show();
+
+    $('#name').val('').focus();
+    $('#score').val('');
+    $('#scores, #results').show();
 }
 
 window.onload = function () {
-    $(document).ready(function() {
-        
-        jQuery.extend(jQuery.expr[':'], {
-            focusable: function(el, index, selector) {
-                return $(el).is('a, button, :input, [tabindex]');
-            }
-        });
-    
-        
-        $(document).on('keypress', 'input,select', function(e) {
+    $(document).ready(function () {
+        $('#name').focus();
+
+        $(document).on('keypress', 'input,select', function (e) {
             if (e.key === "Enter") {
                 e.preventDefault();
-                let $canfocus = $(':focusable');
-                let index = $canfocus.index(this) + 1;
-                if (index >= $canfocus.length) index = 0;
-                $canfocus.eq(index).focus(); // Moves focus to the next element
+                let focusable = $(':focusable');
+                let index = focusable.index(this) + 1;
+                if (index >= focusable.length) index = 0;
+                focusable.eq(index).focus();
             }
         });
-            
-        $('#name').focus();
-    });
-    
-    $('#display_results').on('click',  function() {
-        displayResults();
-    });
 
-    $('#display_scores').on('click',  function() {
-        displayScores();
-    });
-    $('#add').on('click',  function() {
-        addScore();
-    });
+        $('#display_results').on('click', displayResults);
+        $('#display_scores').on('click', displayScores);
+        $('#add').on('click', addScore);
 
-    let name = $('#name');
-    let score = $('#score');
-
-    name.focus();
-    initializeResults();
-    initializeScoresTable();
-}
+        initializeResults();
+        initializeScoresTable();
+    });
+};
